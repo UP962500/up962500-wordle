e237c5e2f33ab16566a6210612702d716d74e1eb
Ahmed Keshka <up962500@myport.ac.uk>
2024-04-27 06:43:57 +0300

    bug fixes

--- public/submit.js
+++ public/submit.js
@@ -1,20 +1,21 @@
-const submit = document.querySelector('.submit');
-const input = document.querySelector('.text-input');
+const submit = document.querySelector(".submit");
+const input = document.querySelector(".text-input");
 
-submit.addEventListener('click', async function submitClick() {
+submit.addEventListener("click", async function submitClick() {
   const data = { val: input.value };
   const word = data.val;
-  const paragraph = document.querySelector('.p-msg');
+  const paragraph = document.querySelector(".p-msg");
+  paragraph.classList.remove("p-green");
 
   const timeOutFunction = () => {
     setTimeout(() => {
-      paragraph.textContent = '';
+      paragraph.textContent = "";
     }, 1500);
-    input.value = '';
+    input.value = "";
   };
 
-  if (word.split('').length !== 5) {
-    paragraph.textContent = 'Your word is not of length 5. Please try again.';
+  if (word.split("").length !== 5) {
+    paragraph.textContent = "Your word is not of length 5. Please try again.";
     timeOutFunction();
     return;
   }
@@ -24,26 +25,31 @@ submit.addEventListener('click', async function submitClick() {
   );
 
   if (response.status === 404) {
-    paragraph.textContent = 'Your word is not valid. Please try again.';
+    paragraph.textContent = "Your word is not valid. Please try again.";
     timeOutFunction();
     return;
   }
 
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   };
 
-  const msg = await fetch('/submit', options);
+  const msg = await fetch("/submit", options);
+
+  console.log("2", msg);
 
   if (!msg.ok) {
-    return console.log('Error!');
+    return console.log("Error!");
   }
 
   const jsonResponse = await msg.json();
+  console.log("hi", jsonResponse.code);
+
+  if (jsonResponse.code === "ADDED") paragraph.classList.add("p-green");
   const resultMessage = jsonResponse.msg;
   paragraph.textContent = resultMessage;
   timeOutFunction();
